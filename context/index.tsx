3736908f5094036710908a8726780ec30a5b10fc
James-wasonga <jameswasonga22@gmail.com>
2025-02-09 22:16:39 +0300

    update client folder

--- context/index.tsx
+++
@@ -1,8 +0,0 @@
-// import React, { useContext, createContext } from 'react';
-// import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
-
-
-// const address = useAddress();
-// const connect = useMetamask();
-
-// const StateContext = createContext();
